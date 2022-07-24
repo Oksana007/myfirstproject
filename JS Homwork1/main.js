349b1560ae6920fc3a3d9f9d8a19203be0003fbf
Oksana007 <o.hnidets27@gmail.com>
2022-07-24 20:31:07 +0300

    new commit

---
+++ JS Homwork1/main.js
@@ -0,0 +1,84 @@
+let h ='hello';
+h = "hello";
+console.log(h);
+
+
+let variable6 = 'owu';
+variable6 = 'owu';
+console.log(variable6);
+
+let adress ='com'
+adress = 'com';
+console.log(adress);
+
+let adress1 = 'ua';
+adress1 = 'ua';
+console.log(adress1);
+
+let foo;
+foo = 1;
+console.log(foo);
+
+let foo2;
+foo2 = 10;
+console.log(foo2);
+
+let foo3;
+foo3= -999;
+console.log(foo3);
+
+let foo4;
+foo4 = 123;
+console.log(foo4);
+
+let foo5;
+foo5 = 3.14;
+console.log(foo5);
+
+let variable;
+ variable= 2.7;
+console.log(variable);
+
+let variable1;
+variable1 = 16;
+console.log(variable1);
+
+let varieble2;
+varieble2 = true;
+console.log(varieble2);
+
+console.log(!!null);
+
+let firstname = 'Oksana';
+console.log(firstname)
+document.write(firstname);
+
+let secondname = 'Hnidets';
+console.log(secondname);
+document.write(secondname);
+
+let lastname = 'Mukolaivna'
+console.log(lastname);
+document.write(lastname);
+
+let person = 'Oksana'+ ' '+ 'Hnidets' + ' ' + 'Mukolaivna';
+console.log(person);
+document.writeln(person);
+
+
+ let a = 100;
+ let b = 100;
+ let c = true;
+
+ console.log(typeof a);
+ console.log(typeof b);
+ console.log(typeof c);
+
+
+
+
+
+
+
+
+
