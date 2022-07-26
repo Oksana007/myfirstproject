ddfa669281b36d4f070f85e6987e2efb1faa0f3f
Oksana007 <o.hnidets27@gmail.com>
2022-07-27 02:11:27 +0300

    JS Home Work 2

---
+++ JS Homework2/main.js
@@ -0,0 +1,69 @@
+let machinery = ['refrigerator', 'TV', 'iron', 'hair dryer', 'kettle', 'washing machine', 'dishwashers',
+  'freezers','sandwich maker','kitchen hood'];
+ console.log(machinery);
+ console.log(machinery [0]);
+ console.log(machinery [1]);
+ console.log(machinery [2]);
+ console.log(machinery [3]);
+ console.log(machinery [4]);
+ console.log(machinery [5]);
+ console.log(machinery [6]);
+ console.log(machinery [7]);
+ console.log(machinery [8]);
+ console.log(machinery [9]);
+
+
+
+ let book1 = {title: 'Jane Eyre', pagecount: 250, genre: 'novel'};
+ console.log(book1);
+ let book2 = {title: 'Martin Eden', pagecount: 180, genre: 'drama'};
+console.log(book2);
+  let book3 = {title: 'White Fang', pagecount: 180, genre: 'story'};
+console.log(book3);
+
+
+
+let bookn1 = {title: 'The Professor', pagecount: 340, genre: 'novel', author:'Sharlota Bronte'};
+console.log(bookn1);
+let bookn2 = {title: 'Gone with The Wind', pagecount: 98, genre: 'novel', author:'Margaret Michells'};
+console.log(bookn2);
+let bookn3 = {title: 'White Fang', pagecount: 180, genre: 'story', author:'Djek London'};
+console.log(bookn3)
+  authors = [
+  {name: bookn1.author, age:45},
+  {name: bookn2.author, age:38},
+  {name: bookn3.author, age: 54}
+];
+console.log(authors);
+
+
+
+
+   let user = [
+      {name:'Masha', username:'Nosa', password:'1234'},
+      {name:'ola', username:'kasa', password:'4567'},
+     {name:'hala', username:'masa', password:'6574'},
+     {name:'lyba', username:'rasa', password:'9999'},
+     {name:'oksana', username:'kasa', password:'8888'},
+     {name:'vasyl', username:'kasa', password:'6666'},
+     {name:'oleh', username:'kasa', password:'5555'},
+     {name:'ivan', username:'kasa', password:'867564'},
+     {name:'serhiy', username:'kasa', password:'101010'},
+     {name:'Sasha', username:'kasa', password:'3456789'},
+   ]
+  console.log(user);
+   console.log(user [0].password);
+   console.log(user [1].password);
+   console.log(user [2].password);
+   console.log(user [3].password);
+   console.log(user [4].password);
+   console.log(user [5].password);
+   console.log(user [6].password);
+   console.log(user [7].password);
+   console.log(user [8].password);
+   console.log(user [9].password);
+
+
+
+
+
